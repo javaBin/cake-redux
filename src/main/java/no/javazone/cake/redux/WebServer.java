a939adec7ed2f570fcbc6f627ab17fb1167f1bee
Anders Karlsen <karlsenanders@hotmail.com>
2014-02-06 22:18:19 +0100

    Added webserver

---
+++ src/main/java/no/javazone/cake/redux/WebServer.java
@@ -0,0 +1,34 @@
+package no.javazone.cake.redux;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ShutdownHandler;
+import org.eclipse.jetty.webapp.WebAppContext;
+
+public class WebServer {
+
+    private final Integer port;
+
+    public WebServer(Integer port) {
+        this.port = port;
+    }
+
+    public static void main(String[] args) throws Exception {
+        new WebServer(getPort(8081)).start();
+    }
+
+    private void start() throws Exception {
+        Server server = new Server(port);
+        HandlerList handlerList = new HandlerList();
+        handlerList.addHandler(new ShutdownHandler("yablayabla", false, true));
+        handlerList.addHandler(new WebAppContext("src/main/webapp", "/"));
+        server.setHandler(handlerList);
+        server.start();
+        System.out.println(server.getURI());
+    }
+
+    private static Integer getPort(int defaultPort) {
+        String envPort = System.getenv("PORT");
+        return Integer.valueOf(envPort == null ? String.valueOf(defaultPort) : envPort);
+    }
+}
