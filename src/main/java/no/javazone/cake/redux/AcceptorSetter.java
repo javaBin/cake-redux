f0ba1f3b64ac31fbead0c94d4f4c79714ca2dbba
Anders Karlsen <karlsenanders@gmail.com>
2014-03-13 16:25:55 +0100

    Using template file to hold message

--- src/main/java/no/javazone/cake/redux/AcceptorSetter.java
+++ src/main/java/no/javazone/cake/redux/AcceptorSetter.java
@@ -6,6 +6,9 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.IOException;
+import java.io.InputStream;
+
 public class AcceptorSetter {
     private EmsCommunicator emsCommunicator;
 
@@ -21,20 +24,40 @@ public class AcceptorSetter {
                 JSONObject jsonTalk = new JSONObject(emsCommunicator.fetchOneTalk(encodedTalkRef));
                 String title = jsonTalk.getString("title");
                 SimpleEmail mail = new SimpleEmail();
-                mail.setMsg("The title is " + title);
-                mail.setFrom("program@java.no","Javazone program commitee");
+                mail.setFrom("program@java.no", "Javazone program commitee");
                 mail.addCc("program@java.no");
                 mail.setSubject("Talk accepted");
                 mail.setHostName(Configuration.smtpServer());
                 mail.setSmtpPort(Configuration.smtpPort());
+
                 JSONArray jsonSpeakers = jsonTalk.getJSONArray("speakers");
+                StringBuilder speakerName=new StringBuilder();
                 for (int j=0;j<jsonSpeakers.length();j++) {
                     JSONObject speaker = jsonSpeakers.getJSONObject(j);
                     String email=speaker.getString("email");
                     String name=speaker.getString("name");
+                    if (!speakerName.toString().isEmpty()) {
+                        speakerName.append(" and ");
+                    }
+
+                    speakerName.append(name);
                     mail.addTo(email);
-                    System.out.println(String.format("Sending mail to %s (%s) acception '%s'",name,email,title));
+                    System.out.println(String.format("Sending mail to %s (%s) acception '%s'", name, email, title));
                 }
+
+                String message;
+
+                try (InputStream is = getClass().getClassLoader().getResourceAsStream("acceptanceTemplate.txt")) {
+                    String template = EmsCommunicator.toString(is);
+                    message = template
+                            .replaceAll("#title#",title)
+                            .replaceAll("#speakername#",speakerName.toString())
+                            ;
+
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+                mail.setMsg(message);
                 mail.send();
 
             } catch (JSONException e) {
