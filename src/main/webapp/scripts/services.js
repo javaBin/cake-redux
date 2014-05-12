391539d17e173054b0382aa0651203d169be7c12
Anders Karlsen <karlsenanders@gmail.com>
2014-05-12 20:41:31 +0200

    Saving filter in cookie

--- src/main/webapp/scripts/services.js
+++ src/main/webapp/scripts/services.js
@@ -126,11 +126,34 @@ angular.module('cakeReduxModule')
 
         }
 
+        var setCookie = function(cname,cvalue,exdays)
+        {
+            var d = new Date();
+            d.setTime(d.getTime()+(exdays*24*60*60*1000));
+            var expires = "expires="+d.toGMTString();
+            document.cookie = cname + "=" + cvalue + "; " + expires;
+        }
+
+        var getCookie = function(cname)
+        {
+            var name = cname + "=";
+            var ca = document.cookie.split(';');
+            for(var i=0; i<ca.length; i++)
+            {
+                var c = ca[i].trim();
+                if (c.indexOf(name)==0) return c.substring(name.length,c.length);
+            }
+            return "[]";
+        }
+
+        var myFilt = JSON.parse(getCookie("cakeFilter"));
+
         var fis = {
-            filters : [],
+            filters : myFilt,
             filterOperators : filterOperators,
             doFilter : function(talks,allTalks) {
                 var self = this;
+                setCookie("cakeFilter",JSON.stringify(self.filters),1);
                 talks.splice(0,talks.length);
                 _.each(allTalks,function(talk) {
                     var res;
