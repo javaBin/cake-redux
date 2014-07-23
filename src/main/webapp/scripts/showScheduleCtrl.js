e064ad8dff963ceb26e848d59bb5ff4bca085fb3
Anders Karlsen <karlsenanders@gmail.com>
2014-07-23 08:32:31 +0200

    Linking listed talks in schedule

--- src/main/webapp/scripts/showScheduleCtrl.js
+++ src/main/webapp/scripts/showScheduleCtrl.js
@@ -1,10 +1,14 @@
 angular.module('cakeReduxModule')
-    .controller('ShowScheduleCtrl', ['$scope','talkList','showScheduleService',
-        function($scope,talkList,showScheduleService) {
+    .controller('ShowScheduleCtrl', ['$scope','talkList','showScheduleService','filterService',
+        function($scope,talkList,showScheduleService,filterService) {
             var sorted = showScheduleService(talkList.talks);
 
             $scope.rooms = sorted.rooms;
             $scope.slots = sorted.slots;
+
+            $scope.talkSelected = function(talk) {
+                filterService.selectedTalk = talk;
+            }
         }]);
 
 
