c8fd25895e4fbf9468c7b7cbcc116e033066a855
Anders Karlsen <karlsenanders@gmail.com>
2015-05-14 15:43:35 +0200

    Showing all info

--- src/main/resources/webapp/scripts/talkReportCtrl.js
+++ src/main/resources/webapp/scripts/talkReportCtrl.js
@@ -2,6 +2,25 @@ angular.module('cakeReduxModule')
     .controller('TalkReportCtrl', ['$scope', '$http', '$routeParams', 'talkList','roomSlotFactory','slotFilterService',
         function($scope, $http, $routeParams,talkList,roomSlotFactory,slotFilterService) {
             $scope.talks = talkList.talks;
+
+            var updateFromServer = function(data) {
+                var atalk = _.findWhere($scope.talks,{ref:data.ref});
+                if (!atalk) {
+                    return;
+                }
+                for (var prop in data) {
+                    atalk[prop] = data[prop];
+                }
+
+
+            };
+
+            $scope.talks.forEach(function(talk) {
+                $http({method: "GET", url: "data/atalk?talkId=" + talk.ref})
+                    .success(updateFromServer);
+            });
+
+
         }
     ]
 );
