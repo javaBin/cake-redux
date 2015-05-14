fb8827132e1b2ef01a66cd9b40a818643039b847
Anders Karlsen <karlsenanders@gmail.com>
2015-05-14 16:40:13 +0200

    Hiding and showing fields

--- src/main/resources/webapp/scripts/talkReportCtrl.js
+++ src/main/resources/webapp/scripts/talkReportCtrl.js
@@ -3,6 +3,28 @@ angular.module('cakeReduxModule')
         function($scope, $http, $routeParams,talkList,roomSlotFactory,slotFilterService) {
             $scope.talks = talkList.talks;
 
+
+            $scope.showTitle = true;
+            $scope.showSummary = true;
+            $scope.showAbstract = true;
+            $scope.showPostcode = true;
+            $scope.showOutline = true;
+            $scope.showKeywords = true;
+            $scope.showAudience = true;
+            $scope.showEquipment = true;
+            $scope.showState = true;
+            $scope.showPublished = true;
+            $scope.showFormat = true;
+            $scope.showLevel = true;
+            $scope.showLanguage = true;
+            $scope.showRoom = true;
+            $scope.showSlot = true;
+            $scope.showSpeakers = true;
+            $scope.showSpeakername = true;
+            $scope.showSpeakerbio = true;
+            $scope.showPostcode = true;
+            $scope.showLastModified = true;
+
             var updateFromServer = function(data) {
                 var atalk = _.findWhere($scope.talks,{ref:data.ref});
                 if (!atalk) {
