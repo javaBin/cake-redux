aec5049e5f6f318c9eb038ade21a4d8ac70aeb9b
Anders Karlsen <karlsenanders@gmail.com>
2016-03-06 21:47:09 +0100

    Cfp mail sender

---
+++ src/test/java/no/javazone/cake/redux/SpeakerMailSender.java
@@ -0,0 +1,112 @@
+package no.javazone.cake.redux;
+
+import org.apache.commons.mail.EmailException;
+import org.apache.commons.mail.SimpleEmail;
+import org.jsonbuddy.JsonArray;
+import org.jsonbuddy.JsonFactory;
+import org.jsonbuddy.JsonObject;
+import org.jsonbuddy.parse.JsonParser;
+
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class SpeakerMailSender {
+    private final String msg;
+    private EmsCommunicator emsCommunicator = new EmsCommunicator();
+
+    public SpeakerMailSender() throws IOException {
+        msg = EmsCommunicator.toString(getClass().getClassLoader().getResourceAsStream("speakerInvMail.txt"));
+    }
+
+    public static void main(String[] args) throws Exception {
+        if (args.length < 1) {
+            System.out.println("Usege SpeakerMailSender <configfile>");
+            return;
+        }
+        System.setProperty("cake-redux-config-file",args[0]);
+        SpeakerMailSender speakerMailSender = new SpeakerMailSender();
+
+        Set<String> allAddresses = speakerMailSender.readSpeakerList();
+        speakerMailSender.sendMailToAll(allAddresses);
+    }
+
+    private void sendMailToAll(Set<String> allAddresses) throws Exception {
+        System.out.println("Sending...");
+        for (String address : allAddresses) {
+            System.out.println(address);
+            sendMail(address);
+            Thread.sleep(2500);
+        }
+    }
+
+    public Set<String> allMailsFrom(String event) {
+        String allTalksStr = emsCommunicator.talksFullVersion(event);
+        JsonArray allTalks = JsonParser.parseToArray(allTalksStr);
+        Set<String> res = allTalks.nodeStream().flatMap(jn -> {
+            JsonObject obj = (JsonObject) jn;
+            return obj.arrayValue("speakers").orElse(JsonFactory.jsonArray()).nodeStream()
+                    .map(speakerNode -> {
+                        JsonObject speakerObject = (JsonObject) speakerNode;
+                        return speakerObject.requiredString("email").toLowerCase();
+                    });
+        }).collect(Collectors.toSet());
+        return res;
+    }
+
+    private SimpleEmail setupMailHeader(SimpleEmail mail, String subject) throws EmailException {
+        mail.setHostName(Configuration.smtpServer());
+        mail.setFrom("program@java.no", "Javazone program commitee");
+        mail.addBcc("program-auto@java.no");
+        mail.setSubject(subject);
+
+
+        if (Configuration.useMailSSL()) {
+            mail.setSSLOnConnect(true);
+            mail.setSslSmtpPort("" + Configuration.smtpPort());
+        } else {
+            mail.setSmtpPort(Configuration.smtpPort());
+
+        }
+        String mailUser = Configuration.mailUser();
+        if (mailUser != null) {
+            mail.setAuthentication(mailUser, Configuration.mailPassword());
+        }
+
+        return mail;
+    }
+
+    private void sendMail(String email) throws EmailException {
+        SimpleEmail mail = new SimpleEmail();
+        mail = setupMailHeader(mail,"JavaZone 2016 Call for Speaker");
+        mail.addTo(email);
+        mail.setMsg(msg);
+        mail.send();
+    }
+
+    private Set<String> readSpeakerList() {
+        JsonArray jsonArray = JsonParser.parseToArray(emsCommunicator.allEvents());
+        Map<String, String> eventmap = jsonArray.nodeStream()
+                .collect(Collectors.toMap(jn -> ((JsonObject) jn).requiredString("slug"), jn -> ((JsonObject) jn).requiredString("ref")));
+        System.out.println(eventmap.keySet());
+        Set<String> speakers2016 = allMailsFrom(eventmap.get("javazone_2016"));
+        Set<String> speakers2015 = allMailsFrom(eventmap.get("javazone_2015"));
+        Set<String> speakers2014 = allMailsFrom(eventmap.get("javazone_2014"));
+        Set<String> speakers2013 = allMailsFrom(eventmap.get("javazone_2013"));
+        System.out.println("2016: " + speakers2016.size());
+        System.out.println("2015: " + speakers2015.size());
+        System.out.println("2014: " + speakers2014.size());
+        System.out.println("2013: " + speakers2013.size());
+
+        SortedSet all = new TreeSet<>();
+        all.addAll(speakers2013);
+        all.addAll(speakers2014);
+        all.addAll(speakers2015);
+        all.removeAll(speakers2016);
+        System.out.println("All:" + all.size());
+
+        all.stream().forEach(System.out::println);
+
+        return all;
+    }
+}
