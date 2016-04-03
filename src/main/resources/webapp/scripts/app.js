6f18bc5a86fe6330b281afacaef9e17bcf8b1521
Anders Karlsen <karlsenanders@gmail.com>
2016-04-03 13:44:56 +0200

    Removed old template and controller

--- src/main/resources/webapp/scripts/app.js
+++ src/main/resources/webapp/scripts/app.js
@@ -5,21 +5,16 @@
 
     angular.module('cakeRedux', ['cakeReduxModule']).
     config(['$routeProvider', function($routeProvider) {
-        $routeProvider.when('/', {
-            templateUrl: 'templates/talkList.html',
-            controller: 'TalkListCtrl'
+        $routeProvider.when("/", {
+            templateUrl: 'templates/talkOverview.html',
+            controller: 'TalkOverviewController'
         }).when("/talks/:eventSlug", {
-            templateUrl: 'templates/talkList.html',
-            controller: 'TalkListCtrl'
+            templateUrl: 'templates/talkOverview.html',
+            controller: 'TalkOverviewController'
         }).when("/showTalk/:talkId", {
             templateUrl: 'templates/showTalk.html',
             controller: 'ShowTalkCtrl'
-        }).when("/list", {
-            templateUrl: 'templates/talkOverview.html',
-            controller: 'TalkOverviewController'
-        }).when("/tlist/:eventSlug", {
-            templateUrl: 'templates/talkOverview.html',
-            controller: 'TalkOverviewController'    
+
         }).when("/accept", {
             templateUrl: 'templates/acceptTalk.html',
             controller: 'AcceptTalkCtrl'
