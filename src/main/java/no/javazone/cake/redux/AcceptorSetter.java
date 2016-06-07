496db3dd77d8b2a60aff16c075d86fc13ac1a21c
Anders Karlsen <karlsenanders@gmail.com>
2016-06-07 19:20:13 +0200

    Subject changed to 2016

--- src/main/java/no/javazone/cake/redux/AcceptorSetter.java
+++ src/main/java/no/javazone/cake/redux/AcceptorSetter.java
@@ -25,7 +25,7 @@ public class AcceptorSetter {
         String template = loadTemplate();
         String tagToAdd = "accepted";
         String tagExistsErrormessage = "Talk is already accepted";
-        String subjectTemplate = "Javazone 2015 #talkType# accepted";
+        String subjectTemplate = "Javazone 2016 #talkType# accepted";
 
         return doUpdates(talks, template, subjectTemplate, tagToAdd, tagExistsErrormessage);
     }
