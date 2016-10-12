48b5e3ca31adba32b8fc5fec5358a6f8939196de
Anders Karlsen <karlsenanders@gmail.com>
2016-10-12 21:00:22 +0200

    Removed debug flag

--- src/main/java/no/javazone/cake/redux/CommunicatorHelper.java
+++ src/main/java/no/javazone/cake/redux/CommunicatorHelper.java
@@ -26,7 +26,7 @@ public final class CommunicatorHelper {
 
     public static InputStream openStream(URLConnection connection) throws IOException {
         InputStream inputStream = connection.getInputStream();
-        if (false) { // flip for debug :)
+        if (true) { // flip for debug :)
             return inputStream;
         }
         String stream = toString(inputStream);
