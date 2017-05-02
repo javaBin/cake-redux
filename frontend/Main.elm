df81decf41618354323824e3f2fd56dcec0e0e9a
Bendik Solheim <bendik@solheim.im>
2017-05-02 19:27:32 +0200

    Slightly more realistic start

--- frontend/Main.elm
+++ frontend/Main.elm
@@ -1,8 +1,42 @@
 module Main exposing (main)
 
-import Html exposing (Html, text)
+import Html exposing (Html, program, text, div)
 
 
-main : Html msg
+type alias Model =
+    { text : String
+    }
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( Model "Hello, world", Cmd.none )
+
+
+type Msg
+    = Placeholder
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    ( model, Cmd.none )
+
+
+view : Model -> Html Msg
+view model =
+    div [] [ text model.text ]
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+main : Program Never Model Msg
 main =
-    text "Hello, world"
+    program
+        { init = init
+        , update = update
+        , view = view
+        , subscriptions = subscriptions
+        }
